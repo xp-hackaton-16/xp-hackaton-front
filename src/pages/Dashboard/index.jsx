4f58996ada54510648ea78c4ca8f5b83ad652ef9
Marcello <oliv.marq.marcello@gmail.com>
2021-01-31 21:34:50 -0300

    dashboard working

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -12,71 +12,56 @@ import {
     EuiLink,
     EuiHealth,  
 } from '@elastic/eui';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { DashboardContainer, StatIcon } from './atoms';
 
 
+const tsToDate = (ts) => {
+  const date = new Date(ts * 1000);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  const seconds = "0" + date.getSeconds();
+
+  return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+}
+
 const columns = [
   {
-    field: 'firstName',
-    name: 'First Name',
+    field: 'event',
+    name: 'ID',
     sortable: true,
-    'data-test-subj': 'firstNameCell',
-    mobileOptions: {
-      render: (item) => (
-        <span>
-          {item.firstName}{' '}
-          <EuiLink href="#" target="_blank">
-            {item.lastName}
-          </EuiLink>
-        </span>
-      ),
-      header: false,
-      truncateText: false,
-      enlarge: true,
-      fullWidth: true,
-    },
-  },
-  {
-    field: 'lastName',
-    name: 'Last Name',
-    truncateText: true,
-    render: (name) => (
-      <EuiLink href={`/incident/${name}`}>
-        {name}
+    render: (event) => (
+      <EuiLink href={`/incident/${event.id}`}>
+        {event.id}
       </EuiLink>
     ),
-    mobileOptions: {
-      show: false,
+  },
+  {
+    field: 'event',
+    name: 'Nome',
+    sortable: true,
+    render: (event) => event.properties.application,
+  },
+  {
+    field: 'event',
+    name: 'Mensagem',
+    render: (event) => event.properties.message,
+  },
+  {
+    field: 'event',
+    name: 'Severidade',
+    render: (event) => {
+      const color = event.properties.severity < 3 ? 'success' : 'danger';
+      return <EuiHealth color={color}>{event.properties.severity}</EuiHealth>;
     },
   },
   {
-    field: 'dateOfBirth',
-    name: 'Date of Birth',
-    dataType: 'date',
-  },
-  {
-    field: 'online',
-    name: 'Online',
-    dataType: 'boolean',
-    render: (online) => {
-      const color = online ? 'success' : 'danger';
-      const label = online ? 'Online' : 'Offline';
-      return <EuiHealth color={color}>{label}</EuiHealth>;
-    },
+    field: 'event',
+    name: 'Data',
+    render: (event) => tsToDate(event.properties.timestamp),
   },
 ];
-
-const items = [
-  {
-    id: '1',
-    firstName: 'john',
-    lastName: 'doe',
-    github: 'johndoe',
-    dateOfBirth: Date.now(),
-    nationality: 'NL',
-    online: true
-  }  
-]
 
 const getRowProps = (item) => {
   const { id } = item;
@@ -99,65 +84,91 @@ const getCellProps = (item, column) => {
 
 
 
-const Dashboard = () => (
-    <DashboardContainer>
-        <EuiPageHeader>
-            <EuiPageHeaderSection>
-                <EuiTitle size="l">
-                    <h1>Dashboard</h1>
-                </EuiTitle>
-            </EuiPageHeaderSection>
-            <EuiPageHeaderSection>
-                <EuiButton>Atualizar</EuiButton>
-            </EuiPageHeaderSection>
-        </EuiPageHeader>
-        <EuiFlexGroup>
-            <EuiFlexItem>
-                <EuiPanel>
-                    <EuiStat
-                        title="125"
-                        description="Incidentes hoje"
-                        textAlign="left">
-                        <StatIcon type="check" color="secondary" />
-                        <EuiTextColor color="secondary">90% corrigido</EuiTextColor>
-                    </EuiStat>
-                </EuiPanel>
-            </EuiFlexItem>
-            <EuiFlexItem>
-                <EuiPanel>
-                    <EuiStat
-                        title="2,000"
-                        description="Incidentes pendentes"
-                        titleColor="accent"
-                        textAlign="left"
-                    />
-                </EuiPanel>
-            </EuiFlexItem>
-            <EuiFlexItem>
-                <EuiPanel>
-                    <EuiStat
-                        title="88"
-                        description="Incidentes críticos"
-                        titleColor="danger"
-                        textAlign="left"
-                    />
-                </EuiPanel>
-            </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiPageHeader style={{ marginTop: 32 }}>
-            <EuiTitle size="m">
-                <h1>Incidentes</h1>
-            </EuiTitle>
-          </EuiPageHeader>
-        <EuiBasicTable
-            items={items}
-            rowHeader="firstName"
-            columns={columns}
-            rowProps={getRowProps}
-            cellProps={getCellProps}
-        />
+const Dashboard = () => {
+  const [data, setData] = useState([]);
 
-    </DashboardContainer>
-);
+  useEffect(() => {
+    axios
+      .get("http://hackatonxp.brazilsouth.azurecontainer.io:4001/v1/issue")
+      .then(response => {
+          setData(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleClick = () => {
+    axios
+      .get("http://hackatonxp.brazilsouth.azurecontainer.io:4001/v1/issue")
+      .then(response => {
+          setData(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+  
+  return (
+      <DashboardContainer>
+          <EuiPageHeader>
+              <EuiPageHeaderSection>
+                  <EuiTitle size="l">
+                      <h1>Dashboard</h1>
+                  </EuiTitle>
+              </EuiPageHeaderSection>
+              <EuiPageHeaderSection onClick={handleClick}>
+                  <EuiButton>Atualizar</EuiButton>
+              </EuiPageHeaderSection>
+          </EuiPageHeader>
+          <EuiFlexGroup>
+              <EuiFlexItem>
+                  <EuiPanel>
+                      <EuiStat
+                          title="125"
+                          description="Incidentes hoje"
+                          textAlign="left">
+                          <StatIcon type="check" color="secondary" />
+                          <EuiTextColor color="secondary">XY% corrigido</EuiTextColor>
+                      </EuiStat>
+                  </EuiPanel>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                  <EuiPanel>
+                      <EuiStat
+                          title={data.length}
+                          description="Incidentes pendentes"
+                          titleColor="accent"
+                          textAlign="left"
+                      />
+                  </EuiPanel>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                  <EuiPanel>
+                      <EuiStat
+                          title="XY"
+                          description="Incidentes críticos"
+                          titleColor="danger"
+                          textAlign="left"
+                      />
+                  </EuiPanel>
+              </EuiFlexItem>
+          </EuiFlexGroup>
+          <EuiPageHeader style={{ marginTop: 32 }}>
+              <EuiTitle size="m">
+                  <h1>Incidentes</h1>
+              </EuiTitle>
+            </EuiPageHeader>
+          <EuiBasicTable
+              items={data}
+              rowHeader="firstName"
+              columns={columns}
+              rowProps={getRowProps}
+              cellProps={getCellProps}
+          />
+
+      </DashboardContainer>
+  );
+}
 
 export default Dashboard;
